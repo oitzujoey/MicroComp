dcaf5fdd144f326808d1752e8db53eab82b20bf4
Joey Herguth <oitzujoey@gmail.com>
2024-10-12 11:26:25 -0700

    Add microcode build script.

---
+++ microcode/build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/sh
+
+if [ -z "$1" ]
+then
+	echo "Provide microcode version like this: v1.2.3"
+	exit
+fi
+
+microcode_version=$1
+
+microasm/microasm -f v1.1.3/microcode-v1.1.3.uasm -o "${microcode_version}/microcode-${microcode_version}-*" -m HEX
+microasm/hex2mem ${microcode_version}/microcode-${microcode_version}-1.txt ${microcode_version}/microcode-${microcode_version}-1.mem
+microasm/hex2mem ${microcode_version}/microcode-${microcode_version}-2.txt ${microcode_version}/microcode-${microcode_version}-2.mem
+microasm/hex2mem ${microcode_version}/microcode-${microcode_version}-3.txt ${microcode_version}/microcode-${microcode_version}-3.mem
+microasm/intel_hex_generator h ${microcode_version}/microcode-${microcode_version}-1.txt ${microcode_version}/microcode-${microcode_version}-1.hex
+microasm/intel_hex_generator h ${microcode_version}/microcode-${microcode_version}-2.txt ${microcode_version}/microcode-${microcode_version}-2.hex
+microasm/intel_hex_generator h ${microcode_version}/microcode-${microcode_version}-3.txt ${microcode_version}/microcode-${microcode_version}-3.hex
