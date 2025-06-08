0d3ba709ed1831294a66fa5aabc50fab73db3edd
Joseph Herguth <oitzujoey@gmail.com>
2025-06-08 10:42:42 -0700

    Make emulator easier to use.

--- Emulator/emulate.sh
+++ Emulator/emulate.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/bash
 
 source=$1
-binary=$2
+binary=$(basename ${source} .hna).bin
+emulation_directory=$(pwd)
 
 pushd "$(dirname "${source}")"
 duck-lisp "$(basename "${source}")"
+cp ${binary} ${emulation_directory}/build/
 popd
 pushd build
-cmake --build . && ./microcomp_emulator $(dirname "${source}")/${binary}
+cmake --build . && ./microcomp_emulator ./${binary}
 popd
